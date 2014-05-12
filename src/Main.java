5f42af072159331fb8a64b59d1c8d6d877423842
kevdez <kernan373@gmail.com>
2014-05-12 14:08:28 -0700

    updated Main parameters to more realistic values

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		double average = 0;
-		double distribution = 2;
+		double average = 200;
+		double distribution = 80;
 		int simulation_steps = 10000;
-		int memory_size = 10000;
+		int memory_size = 5000;
 
 		Driver dr = new Driver(average, distribution, simulation_steps, memory_size);
 		dr.run();
