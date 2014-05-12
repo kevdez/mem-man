ca82b8a0648db5bc14b45cd310cb6d8fad49eca1
kevdez <kernan373@gmail.com>
2014-05-12 04:37:41 -0700

    got rid of PackableMemory. Updated driver and MemManager

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Driver {
@@ -8,18 +8,16 @@ public class Driver {
 //	 what to vary
 	private double avg_req_sz;
 	private double dist_req_sz;
-	private String strategy;
-	
+	private String strategy; 		// "firstfit" or "nextfit"
 	
 //	 what to measure
 	private double avg_mem_util;
 	private double avg_srch_tm;
 	
-	
 	private MemoryManager mem_man;
 	
 //	 to keep track of memory allocations
-	private ArrayList<Integer> list;
+	private LinkedList<Integer> list;
 	
 	Driver(double avg_req_sz, double dist_req_sz, String strategy, MemoryManager mem_man)
 	{
@@ -31,6 +29,8 @@ public class Driver {
 		this.strategy = strategy;
 		
 		this.mem_man = mem_man;
+		
+		this.list = new LinkedList<Integer>();
 	}
 	
 	public void run()
--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -1,17 +1,21 @@
 
-public class MemoryManager extends PackableMemory{
+public class MemoryManager{
 
+	int mem_size;
+	int[] mem;
+	
+	
 	public MemoryManager(int mem_size)
 	{
-		super(mem_size);
-		
+		this.mem_size = mem_size;
+		this.mem = new int[mem_size];
 	}
 	
 	
 //	The three main memory manager functions
 	public void mm_init()
 	{
-		this.mem = new byte[this.size];
+		this.mem = new int[mem_size];
 	}
 	
 	public int mm_request(int n)
