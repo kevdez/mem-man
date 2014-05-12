8ffa52c3a447d65e2dba8484cbb8ab72dcac6402
Kevin Hernandez <khernan3@uci.edu>
2014-05-12 00:51:36 -0700

    added new fields to Driver and Manager classes. Introduced PackableMemory.

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Driver {
+		
+	private Random random;
+	
+//	 what to vary
+	private double avg_req_sz;
+	private double dist_req_sz;
+	private String strategy;
+	
+	
+//	 what to measure
+	private double avg_mem_util;
+	private double avg_srch_tm;
+	
+	
+	private MemoryManager mem_man;
+	
+//	 to keep track of memory allocations
+	private ArrayList<Integer> list;
+	
+	Driver(double avg_req_sz, double dist_req_sz, String strategy, MemoryManager mem_man)
+	{
+		this.random = new Random();
+		
+		this.avg_req_sz = avg_req_sz;
+		this.dist_req_sz = dist_req_sz;
+		
+		this.strategy = strategy;
+		
+		this.mem_man = mem_man;
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	private int getSizeOfNextRequest()
+	{
+		double x = random.nextGaussian();
+		double temp = dist_req_sz * x;
+		return (int) (temp + avg_req_sz);
+	}
 
+	
 }
--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -2,12 +2,29 @@
 public class MemoryManager {
 
 	private int mem_size;
-	private int[] array;
+	private byte[] array;
 
 	public MemoryManager(int mem_size)
 	{
 		this.mem_size = mem_size;
-		this.array = new int[mem_size];
+		this.array = new byte[mem_size];
 	}
 	
+	
+//	The three main memory manager functions
+	public void mm_init()
+	{
+		this.array = new byte[mem_size];
+	}
+	
+	public int mm_request(int n)
+	{
+		
+		return 0;
+	}
+	
+	public void mm_release(int p)
+	{
+		
+	}
 }
