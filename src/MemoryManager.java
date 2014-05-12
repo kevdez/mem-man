efa8a87f1e029cbeee6602c3ebb493a9b9af5085
kevdez <kernan373@gmail.com>
2014-05-11 22:30:38 -0700

    added MemoryManager constructor and fields

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -1,4 +1,13 @@
 
 public class MemoryManager {
 
+	private int mem_size;
+	private int[] array;
+
+	public MemoryManager(int mem_size)
+	{
+		this.mem_size = mem_size;
+		this.array = new int[mem_size];
+	}
+	
 }
